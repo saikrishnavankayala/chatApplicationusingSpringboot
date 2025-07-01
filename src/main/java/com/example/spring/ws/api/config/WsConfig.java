451b177e94640cb92de21155380a73c5c2b68ba3
saikrishnavankayala <saikrishnavankayala9@gmail.com>
2025-07-01 20:12:22 +0530

    Initial commit - Spring Boot WebSocket project

---
+++ src/main/java/com/example/spring/ws/api/config/WsConfig.java
@@ -0,0 +1,31 @@
+package com.example.spring.ws.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WsConfig implements WebSocketMessageBrokerConfigurer{
+	
+	
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		// TODO Auto-generated method stub
+		WebSocketMessageBrokerConfigurer.super.registerStompEndpoints(registry);
+		registry.addEndpoint("/javatechie").withSockJS();
+	}
+    
+	
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry)
+	{
+		WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
+		registry.enableSimpleBroker("/topic");
+		registry.setApplicationDestinationPrefixes("/app");
+	}
+	
+
+}
